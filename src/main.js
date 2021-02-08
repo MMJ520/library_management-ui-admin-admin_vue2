d04f8ef7d04648c2870d5fb2cb57d94a2eaecb3f
mmj <7352728+LOVE_MMJ@user.noreply.gitee.com>
2021-02-08 19:32:35 +0800

    配置element ui环境，完成按需加载

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 引入element ui组件
+import './plugin/element_ui'
 Vue.config.productionTip = false
 
 new Vue({
